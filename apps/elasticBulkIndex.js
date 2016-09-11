16fd49f0ac0b4e4e5effeeaf533aaaac22e33319
Mathieu Mathurin <mathurin05@gmail.com>
2016-09-11 09:47:21 -0400

    updated after bulk index

---
+++ apps/elasticBulkIndex.js
@@ -0,0 +1,28 @@
+var fs = require('fs'),
+    request = require('request-promise');
+
+/*
+JSON structure for bulk tasks
+https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-bulk.html
+*/
+
+var players = fs.readFileSync('./DB/players.json');
+players = JSON.parse(players);
+
+var elasticIndexTask = '{ "index" : { "_index" : "players", "_type" : "player" } }\n'
+var bulkTask = '';
+
+players.forEach(function(player){
+    bulkTask = bulkTask + elasticIndexTask + JSON.stringify(player) + '\n';
+});
+
+var options = {
+    method: 'POST',
+    uri: 'http://localhost:9200/_bulk',
+    body : bulkTask
+};
+
+request(options).then(function(body){
+    console.log(body);
+    process.exit();
+});
