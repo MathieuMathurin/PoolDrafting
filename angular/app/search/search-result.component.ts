5772b3889fefc0a6911d56bd30c96d0782ab0abc
Mathieu Mathurin <mathurin05@gmail.com>
2016-09-17 02:59:28 -0400

    added detailes predictions

--- angular/app/search/search-result.component.ts
+++ angular/app/search/search-result.component.ts
@@ -15,11 +15,16 @@ export class SearchResultComponent implements OnInit {
 
     ngOnInit():void {
         let tempDict: Dictionary;
+        if(this.player.Prediction){
+            let pred = this.player.Prediction;
+            this.stats.push({displayValue: "2016-2017", value: {GP: pred.PJ, G: pred.BTS, A: pred.PAS, W: pred.VIC, L: pred.DEF, OT: pred.DP, SOW: pred.BL, SOL: 0}});
+        }
+
         if(this.player.Stats){
-            Object.keys(this.player.Stats).forEach(key => {
-                this.stats.push({displayValue: key, value: this.player.Stats[key]});
-            });
-            this.stats = this.stats.reverse();
+            this.stats.push({displayValue: "2015-2016", value: this.player.Stats["2015-2016"]});
+            this.stats.push({displayValue: "2014-2015", value: this.player.Stats["2014-2015"]});
+            this.stats.push({displayValue: "2013-2014", value: this.player.Stats["2013-2014"]});
+            this.stats = this.stats;
         }        
     }
 
