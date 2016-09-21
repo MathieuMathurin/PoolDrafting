f17e50e8ab3f69c896ee0c3e7b3bf1769abaed9e
Mathieu Mathurin <mathurin05@gmail.com>
2016-09-21 11:17:06 -0400

    added md-tab + total salary count

--- angular/app/team/team.component.ts
+++ angular/app/team/team.component.ts
@@ -9,49 +9,43 @@ import { MdTab, MdTabGroup, MdTabChangeEvent } from '@angular2-material/tabs/tab
 @Component({
     selector: 'team',
     templateUrl: 'app/team/team.component.html',
-    providers:[
-        UserService        
+    providers: [
+        UserService
     ]
 })
-export class TeamComponent implements OnInit{
-     constructor(private userService: UserService){ }
-    players: Player[];    
-    tabs: Dictionary<{label: string, position: string}> = new Dictionary<{label: string, position: string}>([]);
+export class TeamComponent implements OnInit {
+    constructor(private userService: UserService) { }
+    players: Player[];
+    tabs: Dictionary<{ label: string, position: string }> = new Dictionary<{ label: string, position: string }>([]);
 
     ngOnInit(): void {
         this.players = this.userService.load().players;
 
-        this.tabs.add("all", {label: "Tous les joueurs", position: "all"});
-        this.tabs.add("LW", {label: "Ailiers gauches", position: "LW"});
-        this.tabs.add("C", {label: "Centres", position: "C"});
-        this.tabs.add("RW", {label: "Ailiers droits", position: "RW"});
-        this.tabs.add("D", {label: "Défenseurs", position: "D"});
-        this.tabs.add("G", {label: "Gardiens", position: "G"});
+        this.tabs.add("all", { label: "Tous les joueurs", position: "all" });
+        this.tabs.add("LW", { label: "Ailiers gauches", position: "LW" });
+        this.tabs.add("C", { label: "Centres", position: "C" });
+        this.tabs.add("RW", { label: "Ailiers droits", position: "RW" });
+        this.tabs.add("D", { label: "Défenseurs", position: "D" });
+        this.tabs.add("G", { label: "Gardiens", position: "G" });
     }
 
-    totalSalary(position: string): number{
+    totalSalary(): number {
         let total = 0;
-        if(position === "all"){
-            this.players.forEach(player => total += player.Salary);
-        }else{
-            this.players
-            .filter(player => player.Position === position)
-            .forEach(player => total += player.Salary);
-        }
-        
+        this.players.forEach(player => total += player.Salary);
+
         return total;
     }
-    
-    clear(player: Player){
-        this.userService.update(player, false, () => {this.players = this.players.filter(p => p.Id !== player.Id)});
+
+    clear(player: Player) {
+        this.userService.update(player, false, () => { this.players = this.players.filter(p => p.Id !== player.Id) });
     }
-    
+
     filter(position: string): Player[] {
-        return this.players.filter(player =>  player.Position === position);        
+        return this.players.filter(player => player.Position === position);
     }
 
     count(position: string): number {
-        return this.players.filter(player =>  player.Position === position).length;
-    }    
-        
- }
+        return this.players.filter(player => player.Position === position).length;
+    }
+
+}
