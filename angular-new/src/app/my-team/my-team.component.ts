4c485adafc397921c13221ebf3443d5e33c86f9a
Mathieu Mathurin <mathurin05@gmail.com>
2018-09-21 01:31:10 -0400

    handle pre-draft

--- angular-new/src/app/my-team/my-team.component.ts
+++ angular-new/src/app/my-team/my-team.component.ts
@@ -12,16 +12,22 @@ export class MyTeamComponent implements OnInit {
 
   constructor(private me: MeService, private router: Router) { }
   myPicks: Pick[];
+  myDraftPicks: Pick[];
+  myPreDraftPicks: Pick[];
 
   ngOnInit() {
     this.myPicks = [];
+    this.myDraftPicks = [];
+    this.myPreDraftPicks = [];
     this.getMyPicks();
   }
 
   private getMyPicks = async () => {
     try {
       const myPicks = await this.me.getMyPicks();
-      this.myPicks = myPicks.reverse();
+      this.myPicks = myPicks;
+      this.myPreDraftPicks = myPicks.filter(pick => pick.round === 0);
+      this.myDraftPicks = myPicks.filter(pick => pick.round !== 0).reverse();
     } catch (e) {
       // do nothing since we don't want to loose the loaded data
     }
